2eb70bc853aa60ca8b52834ff79a6ceb4fcb2912
XivC <artemd55131@gmail.com>
2024-10-19 22:44:35 +0300

    Ok

--- add_text.py
+++ add_text.py
@@ -6,11 +6,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def _split_by_halves(text: str):
-    words = text.split(" ")
-    half_len = len(words) // 2
-
-    left, right = " ".join(words[:half_len]), " ".join(words[half_len:])
-    return left, right
+    words = text.split("|")
+    return words[0], words[1]
 
 
 def _add_text_to_img(
@@ -28,11 +25,13 @@ def _add_text_to_img(
     draw = ImageDraw.Draw(text_image)
     _, _, text_width, text_height = draw.textbbox(xy=(0, 0), text=text, font=font)
 
-    x, y = coords[0] - text_width / 2, coords[1] + text_height / 2,
+    x, y = coords[0] - text_width / 2, coords[1] - text_height / 2,
     draw.text((x, y), text, font=font, **text_kwargs)
 
     if sub_processor:
-        text_image = sub_processor(text)
+        stock_size = text_image.size
+        text_image = sub_processor(text_image)
+        text_image = text_image.resize(stock_size)
     return Image.alpha_composite(base_image, text_image)
 
 def add_text(
@@ -43,11 +42,13 @@ def add_text(
 ) -> Image:
     top, bottom = _split_by_halves(text)
     x_top = img.width / 2
-    y_top = 0
+    y_top = img.height * 0.1
     x_bottom = img.width / 2
     y_bottom = img.height * 0.9
 
-    img = _add_text_to_img(img, (x_top, y_top), top, fill="white", stroke_width=3, stroke_fill="black")
-    img = _add_text_to_img(img, (x_bottom, y_bottom), bottom, fill="yellow", stroke_width=3, stroke_fill="black")
+    img = _add_text_to_img(img, (x_top, y_top), top, font_size=font_size, fill="white", sub_processor=sub_processor,
+                           stroke_width=3, stroke_fill="black")
+    img = _add_text_to_img(img, (x_bottom, y_bottom), bottom, font_size=font_size, fill="yellow",
+                           sub_processor=sub_processor, stroke_width=3, stroke_fill="black")
 
     return img
--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from PIL import Image
 
 from add_text import add_text
 from image_transform import apply_seam_carving
+from outline.outline import add_gradient_outline
+from overlay.overlay import overlay_images
+from remove_metadata import remove_background_metadata
 
-test = Image.open("tests/img.png")
+background = Image.open("tests/Dust_II.jpg")
+foreground = Image.open("tests/nikita.png")
 
-test = apply_seam_carving(test, 0.9, 0.5)
-test = add_text(test, "Умоляю тебя выеби меня сковородкой", 30)
+background = remove_background_metadata(background)
+foreground = remove_background_metadata(foreground)
 
-test.show()
+foreground = add_gradient_outline(foreground)
+
+result_image = overlay_images(background, foreground, relative_position=(0.1, 0.5))
+result_image = apply_seam_carving(result_image, 0.8, 0.5)
+result_image = add_text(
+    result_image,
+    "Иду 1:15 | в пизду эту игру",
+    font_size=45,
+    sub_processor=lambda img: add_gradient_outline(img, 10,)
+)
+
+result_image = result_image.resize(background.size)
+result_image.show()
