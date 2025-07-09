3e13bbc4acdd5b408bdc9d9d55ddf3ec7e26a2a8
XivC <artemd55131@gmail.com>
2025-07-10 00:35:04 +0300

    Refactoring

--- automation/processor.py
+++ automation/processor.py
@@ -4,44 +4,8 @@ from typing import Callable, Any
 from PIL import Image
 
 import add_text
-from image_transform import apply_seam_carving
-from outline.outline import add_gradient_outline
-from overlay.overlay import overlay_images
-from ratio import add_noise, change_ratio
-
-
-class OpType(str, Enum):
-    GENERATING = "GENERATING"
-    UNARY = "UNARY"
-    BINARY = "BINARY"
-
-
-class OpCode(str, Enum):
-    TEXT = "text"
-    SEAM_CARVING = "seam_carving"
-    OVERLAY = "overlay"
-    OUTLINE = "outline"
-    NOICE = "noise"
-    RATIO = "ratio"
-
-
-OPERATION_BY_OPCODE: dict[OpCode, Callable] = {
-    OpCode.TEXT: add_text,
-    OpCode.SEAM_CARVING: apply_seam_carving,
-    OpCode.OVERLAY: overlay_images,
-    OpCode.OUTLINE: add_gradient_outline,
-    OpCode.NOICE: add_noise,
-    OpCode.RATIO: change_ratio,
-}
-
-OPCODE_TYPE: dict[OpCode, OpType] = {
-    OpCode.TEXT: OpType.GENERATING,
-    OpCode.SEAM_CARVING: OpType.UNARY,
-    OpCode.OVERLAY: OpType.BINARY,
-    OpCode.OUTLINE: OpType.UNARY,
-    OpCode.NOICE: OpType.UNARY,
-    OpCode.RATIO: OpType.UNARY,
-}
+from memgen.effects import add_gradient_outline, apply_seam_carving, add_noise
+from memgen.utils import overlay_images, change_ratio
 
 
 class ImageProcessor:
--- memgen/effects.py
+++ memgen/effects.py
@@ -1,5 +1,15 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageOps
+import seam_carving
+from PIL import Image, ImageOps, ImageFilter
+
+from .rgb import find_unused_color, to_rgb, to_rgba
+from .utils import change_ratio, crop_image
+
+__all__ = [
+    "add_gradient_outline",
+    "apply_seam_carving",
+    "add_noise",
+]
 
 
 def add_gradient_outline(image: Image.Image, outline_width: int = 20, color: tuple = (255, 255, 0)) -> Image.Image:
@@ -34,3 +44,35 @@ def add_gradient_outline(image: Image.Image, outline_width: int = 20, color: tup
     gradient_outline.paste(image, (outline_width, outline_width), mask=alpha)
 
     return gradient_outline
+
+
+def apply_seam_carving(img: Image, width_scale: float, height_scale: float) -> Image:
+
+    mask = find_unused_color(img)
+
+    rgb_img = to_rgb(img, mask)
+
+    data = np.array(rgb_img)
+    src_h, src_w, _ = data.shape
+    target_h, target_w = int(src_h * height_scale), int(src_w * width_scale)
+
+    transformed = seam_carving.resize(
+        data, (target_w, target_h),
+    )
+    transformed_img = Image.fromarray(transformed, "RGB")
+    transformed_img = to_rgba(transformed_img, mask)
+
+    x_scale = transformed_img.size[0] / img.size[0]
+    y_scale = transformed_img.size[1] / img.size[1]
+
+    return change_ratio(transformed_img, 1/x_scale, 1/y_scale)
+
+
+def add_noise(img: Image, x_scale: float, y_scale: float):
+    width, height = img.size
+    return img.resize(
+        (int(width * x_scale), int(height * y_scale))
+    ).resize((width, height))
+
+
+
